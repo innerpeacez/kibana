eaa3d60df7b8dd03e41d927dab75fb7d75d1f2bf
Catherine Liu <catherine.liu@elastic.co>
2018-10-10 12:14:31 +0100

    Closes workpad manager modal after cloning template
    Fixed filtering workpad templates
    
    Removed console log
    
    Added sample templates

--- x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
+++ x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
@@ -5,7 +5,18 @@
  */
 
 import { cloneDeep } from 'lodash';
-const elasticTemplate = require('./elastic_template.json');
+const canvasWeather = require('./canvas_weather.json');
 const conditionalAssets = require('./conditional_assets.json');
+const elasticTemplate = require('./elastic_template.json');
+const markdownStyles = require('./markdown_styles.json');
+const operationalMonitoring = require('./sample_data_operational_monitoring.json');
+const randomizedTour2018 = require('./randomized_tour_2018.json');
 
-export const templates = [cloneDeep(elasticTemplate), cloneDeep(conditionalAssets)];
+export const templates = [
+  cloneDeep(canvasWeather),
+  cloneDeep(conditionalAssets),
+  cloneDeep(elasticTemplate),
+  cloneDeep(markdownStyles),
+  cloneDeep(operationalMonitoring),
+  cloneDeep(randomizedTour2018),
+];
